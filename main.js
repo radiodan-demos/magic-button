697e2a34e882eaf2c31ac788543e0d578ecd7e32
Dan Nuttall <pixelblend@gmail.com>
2014-03-15 06:56:45 +0000

    Add web server

---
+++ main.js
@@ -0,0 +1,22 @@
+var express          = require("express"),
+    connect          = require("connect"),
+    http             = require("http"),
+    utils            = require("radiodan-client").utils,
+    logger           = utils.logger(__filename),
+    port             = (process.env.PORT || 5000),
+    server           = module.exports = express();
+
+server.configure(function() {
+  server.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  server.use(connect.methodOverride());
+  server.use(connect.urlencoded());
+  server.use(connect.json());
+  server.use(server.router);
+  server.use(connect.static("public"));
+});
+
+http.createServer(server).listen(port);
+logger.info("Started server on port", port);
