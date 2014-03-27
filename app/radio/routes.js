3622d4962120d89cec41ae3dc810d6058232fcf9
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-03-27 09:43:50 +0000

    Remove unusued code

--- app/radio/routes.js
+++ app/radio/routes.js
@@ -140,38 +140,5 @@ function routes(app, eventBus, radiodan, states, services, bbcServices) {
     };
   }
 
-  // var avoidPlayer    = radiodan.player.get("avoider"),
-  //     mainPlayer     = radiodan.player.get("main"),
-  //     announcePlayer = radiodan.player.get("announcer"),
-  //     audio          = radiodan.audio.get("default"),
-  //     currentServiceId;
-
-  //   app.get("/playing", playService);
-
-  //   // Broadcast along event stream
-  //   bbcServices.on('nowPlaying', function (service, data) {
-  //     eventStream.send(data, 'nowPlaying');
-  //   });
-
-  //   function playService(req, res) {
-  //     var id  = req.query.id,
-  //         service = bbcServices.get(id),
-  //         url;
-
-  //     if (service) {
-  //       url = service.audioStreams[0].url;
-  //     }
-
-  //     if (url) {
-  //       mainPlayer.clear();
-  //       mainPlayer.add({ playlist: [url] });
-  //       mainPlayer.play();
-
-  //       currentServiceId = id;
-  //     }
-
-  //     res.redirect('back');
-  //   };
-
     return app;
 }
