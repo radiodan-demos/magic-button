c28af041049ad0c5b122ba5b598a5830d9769af4
Dan Nuttall <pixelblend@gmail.com>
2014-04-01 16:20:26 +0100

    Add boilerplate for announcer route

--- app/announcer/routes.js
+++ app/announcer/routes.js
@@ -1,21 +1,43 @@
-var utils    = require("radiodan-client").utils,
-    settingsRoutes = require("../settings/routes"),
+var utils    = require('radiodan-client').utils,
+    settingsRoutes = require('../settings/routes'),
     logger   = utils.logger(__filename);
 
 module.exports = routes;
 
 function routes(app, states, Settings) {
   var settings = Settings.build(
-        "announcer",
-        { speak: false, announcer: "marvin" }
+        'announcer',
+        { speak: false, announcer: 'marvin' }
       );
 
-  app.get("/", index);
+  app.get('/', index);
+  app.get('/state.json', state);
+  app.post('/', announce);
+  app.delete('/', cancel);
+
   app.use(settingsRoutes(settings));
 
   return app;
 
   function index(req, res) {
-    res.json({page: "Announcer"});
+    res.json({page: 'Announcer'});
+  }
+
+  function state(req, res) {
+    res.json({
+      isAnnouncing: (states.state === 'announcing')
+    });
+  }
+
+  function announce(req, res) {
+    settings.get().then(function(settings) {
+      states.handle('startAnnouncing', settings);
+      res.redirect('./');
+    }).then(null, utils.failedPromiseHandler(logger));
+  }
+
+  function cancel(req, res) {
+    states.handle('stopAnnouncing');
+    res.redirect('./');
   }
 }
