25e818c74eddf79c6a6ba11d59a4620c9a0b5c9a
Dan Nuttall <pixelblend@gmail.com>
2014-04-07 12:56:45 +0100

    Pendantic quote fixing

--- app/events/routes.js
+++ app/events/routes.js
@@ -1,11 +1,12 @@
-var eventSource = require("express-eventsource");
+var eventSource = require('express-eventsource'),
+    logger      = require('radiodan-client').utils.logger('event-routes');
 
 module.exports = function (app, eventBus, services) {
   var eventStream = eventSource();
 
   // Shared eventsource
   // To send data call: eventStream.send(dataObj, 'eventName');
-  app.use("/", eventStream.middleware());
+  app.use('/', eventStream.middleware());
 
   services.bbc.on('*', function (data) {
     eventStream.send(data);
@@ -15,6 +16,7 @@ module.exports = function (app, eventBus, services) {
     if (args.length === 1) {
       args = args[0];
     }
+
     eventStream.send({ topic: eventName, data: args });
   });
 
