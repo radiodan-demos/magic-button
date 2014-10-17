b2b6b29768567fd69c5f68ef61c51da772eb2ad3
Dan Nuttall <pixelblend@gmail.com>
2014-10-17 13:17:40 +0100

    Add metadata requests to service register

--- test/lib/test-services-register.js
+++ test/lib/test-services-register.js
@@ -1,21 +1,10 @@
-/* globals describe, it, before */
-'use strict';
-
-var chai = require('chai'),
-    assert = chai.assert,
-    chaiAsPromised = require('chai-as-promised'),
-    sinon  = require('sinon');
-
-var utils = require('radiodan-client').utils;
-
 var ServicesRegister = require('../../lib/services-register');
-
-chai.use(chaiAsPromised);
 
 describe('ServicesRegister', function(){
   it('registers a service provider', function(done){
     var subject = ServicesRegister.create(),
-        fetchServices = function() { return utils.promise.resolve([]);},
+        servicesPromise = utils.promise.resolve([]),
+        fetchServices = function() { return servicesPromise; },
         source  = { id: 'testSource', fetchServices: fetchServices };
 
     var promise = subject.register(source);
@@ -29,7 +18,8 @@ describe('ServicesRegister', function(){
 
   it('registers services to a provider', function(done) {
     var subject = ServicesRegister.create(),
-        fetchServices = function() { return utils.promise.resolve(['testService']);},
+        servicesPromise = utils.promise.resolve(['testService']),
+        fetchServices = function() { return servicesPromise;},
         source  = { id: 'testSource', fetchServices: fetchServices };
 
     var promise = subject.register(source);
@@ -39,6 +29,23 @@ describe('ServicesRegister', function(){
         assert.deepEqual(subject.services(), ['testService'])
       })
       .then(done, done);
+  });
+
+  it('prevents a provider from being registered twice', function(done) {
+     var subject = ServicesRegister.create(),
+        servicesPromise = utils.promise.resolve(['testService']),
+        fetchServices = function() { return servicesPromise;},
+        source  = { id: 'testSource', fetchServices: fetchServices },
+        firstRegister, secondRegister;
+
+    firstRegister = subject.register(source);
+
+    assert.isFulfilled(firstRegister).then(function() {
+      secondRegister = subject.register(source);
+
+      return assert.isRejected(secondRegister,
+        'Provider testService already registered').notify(done);
+    });
   });
 
   it('prevents a service from being registered twice', function(done) {
@@ -57,5 +64,24 @@ describe('ServicesRegister', function(){
 
       return assert.isRejected(secondRegister).notify(done);
     });
- });
+  });
+
+  it('defers metadata requests to service provider', function(done) {
+    var subject = ServicesRegister.create(),
+        servicesPromise = utils.promise.resolve(['testService']),
+        fetchServices = function() { return servicesPromise;},
+        metadataSpy = sinon.spy(),
+        source  = {
+          id: 'testSource', fetchServices: fetchServices,
+          metadata: metadataSpy
+        },
+        firstRegister, secondRegister;
+
+    register = subject.register(source);
+
+    assert.isFulfilled(register).then(function() {
+      subject.metadata('testService');
+      assert.equal(metadataSpy.callCount, 1);
+    }).then(done,done);
+  });
 });
