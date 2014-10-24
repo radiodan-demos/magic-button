4cf5ec2478543deb380418c1dda3c36f2404d74f
Dan Nuttall <pixelblend@gmail.com>
2014-10-24 14:12:51 +0100

    Add hook for actions to state.
    The state machine needs to be able to handle any number of actions when
    online. It can now accept an object of actions that are attached to the
    online state hander. Next step will be to add an action object that
    registers new actions and marshalls them into this object structure.

--- test/lib/test-state.js
+++ test/lib/test-state.js
@@ -1,6 +1,6 @@
 var State = require(libDir + 'state');
 
-describe('state', function() {
+describe('State', function() {
   beforeEach(function() {
     this.subject = State.create();
   });
@@ -49,6 +49,33 @@ describe('state', function() {
     it('cannot transition', function(){
       this.subject.handle('online');
       assert.equal(this.subject.state, 'shutdown');
+    });
+  });
+
+  describe('adding actions', function() {
+    beforeEach(function(){
+      this.actionStub = sinon.stub(),
+      this.actions = {'testAction': this.actionStub},
+      this.subject = State.create(this.actions);
+    });
+
+    it('adds transitions to online state', function() {
+      this.subject.state = 'online';
+      this.subject.handle('testAction');
+
+      assert.equal(this.actionStub.callCount, 1);
+    });
+
+    it('does not add transitions to standby', function() {
+      this.subject.state = 'standby';
+      this.subject.handle('testAction');
+      assert.equal(this.actionStub.callCount, 0);
+    });
+
+    it('does not add transitions to shutdown', function() {
+      this.subject.state = 'shutdown';
+      this.subject.handle('testAction');
+      assert.equal(this.actionStub.callCount, 0);
     });
   });
 });
